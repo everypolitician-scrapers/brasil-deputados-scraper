cfe83e216d198cefba7ea8e8758157026998b877
Tony Bowden <tony@tmtm.com>
2017-02-07 12:26:49 +0000

    Clean out database on each run

--- scraper.rb
+++ scraper.rb
@@ -17,6 +17,8 @@ end
 @BASE = 'http://www.camara.gov.br/internet/deputado/'
 @url_t = @BASE + 'Dep_Lista.asp?Legislatura=%d&Partido=QQ&SX=QQ&Todos=None&UF=QQ&condic=QQ&forma=lista&nome=&ordem=nome&origem=None'
 
+ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
+
 # (41..55).to_a.reverse.each do |term|
 (55..55).to_a.reverse.each do |term|
   url = @url_t % term

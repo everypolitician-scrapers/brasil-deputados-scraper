80ad7b2282493f198b527696cff11ae7e3d5c677
Tony Bowden <tony@tmtm.com>
2017-02-07 12:26:35 +0000

    require Scraped
    We don't actually _use_ it yet, but it provides us with String.tidy and Nokogiri

--- scraper.rb
+++ scraper.rb
@@ -2,9 +2,8 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
-require 'nokogiri'
-require 'open-uri'
 require 'pry'
+require 'scraped'
 require 'scraperwiki'
 
 require 'open-uri/cached'
@@ -18,7 +17,7 @@ end
 @BASE = 'http://www.camara.gov.br/internet/deputado/'
 @url_t = @BASE + 'Dep_Lista.asp?Legislatura=%d&Partido=QQ&SX=QQ&Todos=None&UF=QQ&condic=QQ&forma=lista&nome=&ordem=nome&origem=None'
 
-#  (41..55).to_a.reverse.each do |term|
+# (41..55).to_a.reverse.each do |term|
 (55..55).to_a.reverse.each do |term|
   url = @url_t % term
   puts "Getting #{url}"
@@ -32,17 +31,17 @@ end
 
     binding.pry if dep_url.include? '189171'
     block = dep.at_css('div.bloco')
-    partido = block.xpath('.//li/strong[contains(.,"Partido")]/../text()').text.strip
+    partido = block.xpath('.//li/strong[contains(.,"Partido")]/../text()').text.tidy
     data = {
       id:           deplink.text[/id=(\d+)/, 1],
-      name:         dep.at_css('div#portal-mainsection h2').text.gsub(/Deputad[ao] /, '').strip,
-      fullname:     block.xpath('.//li/strong[contains(.,"Nome civil")]/../text()').text.strip,
-      party:        partido.split('/')[0].strip,
-      party_id:     partido.split('/')[0].strip,
-      district:     partido.split('/')[1].strip,
-      birth_date:   dep.xpath('//span[contains(.,"Nascimento:")]//following-sibling::strong').text.strip,
-      phone:        block.xpath('.//li/strong[contains(.,"Telefone")]/../text()').text.strip,
-      legislaturas: block.xpath('.//li/strong[contains(.,"Legislaturas")]/../text()').text.strip,
+      name:         dep.at_css('div#portal-mainsection h2').text.gsub(/Deputad[ao] /, '').tidy,
+      fullname:     block.xpath('.//li/strong[contains(.,"Nome civil")]/../text()').text.tidy,
+      party:        partido.split('/')[0].tidy,
+      party_id:     partido.split('/')[0].tidy,
+      district:     partido.split('/')[1].tidy,
+      birth_date:   dep.xpath('//span[contains(.,"Nascimento:")]//following-sibling::strong').text.tidy,
+      phone:        block.xpath('.//li/strong[contains(.,"Telefone")]/../text()').text.tidy,
+      legislaturas: block.xpath('.//li/strong[contains(.,"Legislaturas")]/../text()').text.tidy,
       image:        block.xpath('.//img[contains(@src, "deputado")]/@src').text,
       email:        dep.xpath("//div/ul/li/a[starts-with(@href, 'mailto:')]").inner_html,
       term:         term,
